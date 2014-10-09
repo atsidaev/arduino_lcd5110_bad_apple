d022b7d08aa731596f33f8bac560a969ea643e9a
Alexander Tsidaev <a.tsidaev@gmail.com>
2014-10-09 16:58:33 +0600

    Initial commit

---
+++ Client.ino
@@ -0,0 +1,70 @@
+#include <LCD5110_Graph.h>
+
+LCD5110 myGLCD(8,9,10,11,12);
+
+#define BUF_SIZE 504
+
+uint8_t buf[BUF_SIZE];
+
+int pos = 0;
+
+void setup()
+{
+	Serial.begin(115200);
+
+	myGLCD.InitLCD();
+	randomSeed(analogRead(7));
+
+	clear();
+
+	while (Serial.available())
+	Serial.read();
+
+	Serial.write("1"); // Tell server that we are ready to receive
+}
+
+void clear()
+{
+	for (int i = 0; i < BUF_SIZE; i++)
+		buf[i] = 0; 
+}
+
+// Analog of LCD5110.drawBitmap, but works with RAM arrays instead of ROM ones
+void drawBitmap(LCD5110* lcd, int x, int y, uint8_t* bitmap, int sx, int sy)
+{
+	int bit;
+	byte data;
+
+	for (int cy=0; cy<sy; cy++)
+	{
+		bit= cy % 8;
+		for(int cx=0; cx<sx; cx++)
+		{
+			data=bitmap[cx+((cy/8)*sx)];
+			if ((data & (1<<bit))>0)
+				lcd->setPixel(x+cx, y+cy);
+			else
+				lcd->clrPixel(x+cx, y+cy);
+		}
+	}
+}
+
+void loop()
+{
+	if (pos < BUF_SIZE)
+		return;
+	
+	drawBitmap(&myGLCD, 0, 0, buf, 84, 48);
+
+	myGLCD.update();
+	pos = 0;
+}
+
+void serialEvent()
+{
+	while (Serial.available()) {
+	byte b = Serial.read();
+	if (pos < BUF_SIZE)
+		buf[pos++] = b;
+	}
+}
---
+++ Server.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace bad_apple_server
+{
+	class Server
+	{
+		const string ImagePath = @"images";
+		const string VideoFile = @"tohou.mp4"; // Bad apple!! original video (03:39 length)
+		const int FPS = 30;
+
+		const string PortName = "COM3";
+
+		static void Main(string[] args)
+		{
+			var files = Directory.EnumerateFiles(ImagePath).ToList();
+			var images = files.Select(File.ReadAllBytes).ToArray();
+
+			WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer();
+			player.settings.autoStart = false;
+			player.settings.playCount = 1000;
+			player.URL = VideoFile;
+			Thread.Sleep(1000); // let wmp to finish all the loading shit
+
+			var com = new System.IO.Ports.SerialPort(PortName, 115200, System.IO.Ports.Parity.None, 8, System.IO.Ports.StopBits.One);
+			com.Open();
+
+			int currentImage = 0;
+			bool isPlaying = false;
+
+			while (currentImage < images.Length)
+			{
+				com.ReadByte();
+
+				if (!isPlaying)
+				{
+					player.controls.play();
+					isPlaying = true;
+				}
+
+				var currentPosition = player.controls.currentPosition;
+
+				currentImage = (int)(FPS * currentPosition);
+				com.Write(images[Math.Min(currentImage, images.Length - 1)], 0, 504);
+			}
+			
+			com.Close();
+		}
+	}
+}
